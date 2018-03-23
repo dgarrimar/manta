ddf850d815a4054704e844f8d07b2adb3d53b325
dgarrimar <dgarrimar@gmail.com>
2018-03-23 19:33:06 +0100

    ci trial with md5sum #3

--- ci/run.sh
+++ ci/run.sh
@@ -7,5 +7,5 @@ MD5SUM="8d7ecdf761f2241346a7859251f7877f"
 
 # run mlm2 on sample dataset
 R CMD INSTALL --preclean --no-multiarch --with-keep.source ../mlm
-R -e "library(mlm); fit <- mlm2(biomarkers ~ .^2, data = patients); save(fit, file = 'data-raw/fit.rda')"
-[[ $(md5sum data-raw/fit.rda | awk '{$0=$1}1') == $MD5SUM ]]
+R -e "library(mlm); fit <- mlm2(biomarkers ~ .^2, data = patients); save(fit, file = '/tmp/fit.rda')"
+[[ $(md5sum /tmp/fit.rda | awk '{$0=$1}1') == $MD5SUM ]]
