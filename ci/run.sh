932be55534cdaae771514c54af345c7e4d897321
dgarrimar <dgarrimar@gmail.com>
2018-03-23 20:41:00 +0100

    ci trial with md5sum #11

--- ci/run.sh
+++ ci/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # pre-computed checksum
-MD5SUM="85ec4dd714957756413a834a7a3842f6"
+MD5SUM="85ec4dd714957756413a834a7a3842f6" # Obtained with R vs 3.4.1 (same as in Travis)
 
 # run mlm2 on sample dataset
-# R CMD INSTALL --preclean --no-multiarch --with-keep.source ../mlm
+R CMD INSTALL --preclean --no-multiarch --with-keep.source ../mlm
 R -e "library(mlm); fit <- mlm2(biomarkers ~ .^2, data = patients); save(fit, file = 'ci/fit.rda')"
 [[ $(md5sum ci/fit.rda | awk '{$0=$1}1') == $MD5SUM ]]
