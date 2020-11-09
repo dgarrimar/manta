f2352b07b8a36f82ed6c10613be08b70292b4b1e
dgarrimar <dgarrimar@gmail.com>
2020-11-09 10:52:59 +0100

    upgrade R to fix issue in container for ci

--- ci/run.sh
+++ ci/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Pre-computed checksum obtained using Docker and R vs 3.5.2 (as in Travis)
+# Pre-computed checksum obtained using Docker and R vs 4.0.3 (as in Travis)
 
-MD5SUM="55a77b1fc36fcef10550ca449d66ea59"
+MD5SUM="ca5fe1a0e1e63633051d091277f438ce"
 
 # Install and run mlm on sample dataset
 
 COMM1='R CMD INSTALL --preclean --no-multiarch --with-keep.source ../mlm'
 COMM2='R --vanilla -e "library(mlm); res <- mlm(biomarkers ~ .^2, data = patients); save(res, file = \"ci/res.rda\")"'
 
-docker run --rm --entrypoint /bin/bash -w $PWD -v $PWD:$PWD dgarrimar/mlm:r3.5.2 -c "$COMM1;$COMM2"
+docker run --rm --entrypoint /bin/bash -w $PWD -v $PWD:$PWD dgarrimar/mlm:r4.0.3 -c "$COMM1;$COMM2"
 
 [[ $(md5sum ci/res.rda | awk '{$0=$1}1') == $MD5SUM ]]
