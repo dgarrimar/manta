a9172ed57d7c44a8802b2ad81346d74b8ed573bd
dgarrimar <dgarrimar@gmail.com>
2020-11-12 12:01:56 +0100

    require R >=3.3.2, use testthat for ci

--- ci/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-Rscript --vanilla -e "library(mlm); res <- mlm(biomarkers ~ .^2, data = patients); print.default(res)" > ci/res
-
-if [[ $(diff ci/res ci/res0) ]]; then exit 1; fi
--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -1,1 +1,6 @@
+context("Results on example dataset")
 
+test_that("Current output matches expected results", {
+  fit <- mlm(biomarkers ~ .^2, data = patients, fit = TRUE)
+  expect_equal_to_reference(fit, "example_ref.rds")
+})
