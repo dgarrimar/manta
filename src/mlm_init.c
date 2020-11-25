8989242d985a8c5c396ca7a693bf1c6a169232f7
dgarrimar <dgarrimar@gmail.com>
2020-11-25 20:35:02 +0100

    register native routines, disable symbol search, unload

---
+++ src/mlm_init.c
@@ -0,0 +1,19 @@
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .C calls */
+extern void ruben(double *, int *, double *, int *, double *, double *, int *, double *, double *, int *, double *);
+
+static const R_CMethodDef CEntries[] = {
+  {"ruben", (DL_FUNC) &ruben, 11},
+  {NULL, NULL, 0}
+};
+
+void R_init_mlm(DllInfo *dll) {
+  R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
+
+void R_unload_mlm(DllInfo *info) {
+    /* Release resources. */
+}
